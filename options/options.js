a73d52ccbf525e2202729d09c8f99fd2e32adadf
Crowdbotics <team@crowdbotics.com>
2024-03-28 02:09:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-smoke-47312",
+  url: "https://divine-smoke-47312.botics.co",
+  api: "https://divine-smoke-47312.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
